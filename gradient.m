c15f01742b4e25088d56716a18167f654475add1
MarionWia <marion.wiatrowski@sfr.fr>
2015-02-18 16:25:00 +0100

    calcul gradients

---
+++ gradient.m
@@ -0,0 +1,16 @@
+% ----- Calcul du gradient avec l'information d'intensite
+
+function [ gradI ] = gradient( intensity_matrix )
+
+% On applique les filtres Sx et Sy : filtres de Sobel
+
+Sx = [1 0 -1 ; 2 0 -2 ; 1 0 -1];
+Sy = [1 2 1 ; 0 0 0 ; -1 -2 -1];
+
+Gx = conv2(Sx,intensity_matrix);
+Gy = conv2(Sy,intensity_matrix);
+
+gradI = sqrt((Gx.*Gx)+(Gy.*Gy)); % module du gradient
+
+end
+
