d3d7c1b96aae4dfa56532723405b9fa29ee32519
MarionWia <marion.wiatrowski@sfr.fr>
2015-04-21 09:35:47 +0200

    gradient

--- gradient_sobel.m
+++ gradient_sobel.m
@@ -1,6 +1,6 @@
 % ----- Calcul du gradient avec l'information d'intensite
 
-function [ grad_x , grad_y ] = gradient_sobel( grey_image )
+function [ grad_Ix1 , grad_Iy1 ] = gradient_sobel( grey_image )
 
 h  = fspecial('sobel');
 grad_y = imfilter( grey_image , h  , 'replicate' );
@@ -16,15 +16,15 @@ grad_x = imfilter( grey_image , h' , 'replicate' );
 
 % gradI = [gradIx ; gradIy]; % vecteur gradient 1*2
 
-Enlever les bords qui ont été rajouté avec le calcul du gradient
+% Enlever les bords qui ont été rajouté avec le calcul du gradient
 tailleGrad = size(gradIx);
 gradIxDim = grad_x(2:(tailleGrad(1,1)-1) , 2:(tailleGrad(1,2)-1));
 dim = size(gradIxDim);
 gradIyDim = grad_y(2:(tailleGrad(1,1)-1) , 2:(tailleGrad(1,2)-1));
 
 % Modifier la taille de la matrice: créer un vecteur ligne
-gradIx1 = reshape(gradIxDim,dim(1,1)*dim(1,2),1);
-gradIy1 = reshape(gradIyDim,dim(1,1)*dim(1,2),1);
+grad_Ix1 = reshape(gradIxDim,dim(1,1)*dim(1,2),1);
+grad_Iy1 = reshape(gradIyDim,dim(1,1)*dim(1,2),1);
 
 
 end
